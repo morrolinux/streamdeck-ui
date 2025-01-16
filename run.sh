7623ddf81777acf0dd9c85156e75e168d447df07
Moreno Razzoli <morrolinux@gmail.com>
2025-01-16 10:04:07 +0100

    update startup script to work on my fedora setup

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-source /opt/anaconda/bin/activate root
-conda activate streamdeck
+source ~/.bashrc
+conda
 cd ~/streamdeck-ui 
-
-python3 -m poetry run streamdeck &
-sleep 3
-kill $(pgrep -f "streamdeck/bin/python")
-python3 -m poetry run streamdeck
+sleep 5
+conda activate streamdeck
+poetry run streamdeck &
